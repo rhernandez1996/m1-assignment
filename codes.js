1dade1a84461ed32a725a3ad1f0a7aa96581c22c
rhernandez1996 <rayvenhernandez14@yahoo.com>
2022-09-27 20:11:23 -0700

    finished

--- codes.js
+++ codes.js
@@ -1,21 +1,20 @@
- var checkPhone = document.getElementById("phone");
-var phoneMe = document.getElementById("phoneMe"); 
+var whichOption = document.getElementById("contacting");
+whichOption.addEventListener("change", showHideDiv);
+
+var checkPhone = document.getElementById("phone");
+var phoneMe = document.getElementById("phoneMe");
 var checkEmail = document.getElementById("email");
 var emailMe = document.getElementById("emailMe");
 
-function showFunction1() {
- 
-
-  if (checkPhone.clicked) {
+function showHideDiv() {
+  if (checkEmail.selected) {
+    emailMe.style.display = "block";
+    phoneMe.style.display = "none";
+  } else if (checkPhone.selected) {
+    emailMe.style.display = "none";
     phoneMe.style.display = "block";
   } else {
     emailMe.style.display = "none";
-  }
-}
-function showFunction2() {
-  if (checkEmail.checked) {
-    emailMe.style.display = "block";
-  } else {
     phoneMe.style.display = "none";
   }
 }
